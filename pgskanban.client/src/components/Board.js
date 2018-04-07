af988b5eddd9001e87b044be9ce134dc6a715796
mkurpinski <michal.kurpinski96@gmail.com>
2018-04-07 15:21:00 +0200

    Workshops 4 - connecting frontend with backend

--- pgskanban.client/src/components/Board.js
+++ pgskanban.client/src/components/Board.js
@@ -1,43 +1,53 @@
-import React from 'react';
-import List from './List';
-import FakeData from '../FakeData';
-import './Board.css';
+import React from "react";
+import List from "./List";
+import { BASE_URL } from "../constants";
+import { withRouter } from 'react-router-dom'
+import axios from "axios";
+import "./Board.css";
 
-export default class Board extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            boardName: 'Best board ever!',
-            boardData: [],
-        }
-    }
+class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      boardName: "",
+      boardId: 0,
+      boardData: []
+    };
+  }
 
-    componentDidMount() {
-        this.setState({boardData: FakeData})
-    }
+  componentDidMount() {
+    axios.get(BASE_URL + "/board").then(response => {
+      console.log(response);
+      this.setState({
+        boardData: response.data.lists,
+        boardName: response.data.name,
+        boardId: response.data.id
+      })
+    }).catch(() => {
+      this.props.history.push('/new');
+  });
+  }
 
-    renderLists = () => {
-        return(
-            this.state.boardData.map(
-                (list) => <List listName={list.listName} cards={list.cards} />
-                )
-        )
-    }
+  renderLists = () => {
+    return this.state.boardData.map(list => (
+      <List listName={list.name} cards={[]} />
+    ));
+  };
 
-    render() {
-        return(
-            <div>
-                <div>
-                    <h1>{this.state.boardName}</h1>
-                    <button className="btn btn-info">Add new list</button>               
-                    <input type="text" className="listName__input"/>
-                </div>
-                <div className="container-fluid">
-                    <div className="row flex-row flex-nowrap">
-                        {this.renderLists()}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>{this.state.boardName}</h1>
+          <button className="btn btn-info">Add new list</button>
+          <input type="text" className="listName__input" />
+        </div>
+        <div className="container-fluid">
+          <div className="row flex-row flex-nowrap">{this.renderLists()}</div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default withRouter(Board)
